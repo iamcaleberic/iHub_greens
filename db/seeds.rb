69e5a0e6814a9b0159469ef2fc9f94eae5cb7f51
Eric Caleb <calebme@mail.com>
2016-05-13 17:23:40 +0100

    referances

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+	Admin.create(email:"calebme@mail.com", password: "userpass")
+	Green.create(email:"calebme@mail.com", password: "userpass")
